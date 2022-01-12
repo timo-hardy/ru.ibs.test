b57f523d86c375ec457d0f38dc65dc084d91ccbb
TKhidirov <TKhidirov@IBS.RU>
2022-01-12 11:06:40 +0300

    git init

---
+++ src/test/java/ru/ibs/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibs.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

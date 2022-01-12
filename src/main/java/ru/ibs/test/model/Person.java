1ad8fa19511ef6e1c6c0a6f4f0df125e7d1abd8f
TKhidirov <TKhidirov@IBS.RU>
2022-01-12 11:19:59 +0300

    Create fields in Person.java

--- src/main/java/ru/ibs/test/model/Person.java
+++ src/main/java/ru/ibs/test/model/Person.java
@@ -1,4 +1,21 @@
 package ru.ibs.test.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long personId;
+    private String surname;
+    private String name;
+    private String patronymic;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDate dateOfBirth;
+    private Department department;
 }
